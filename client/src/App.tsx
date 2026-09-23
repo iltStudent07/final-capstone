2ee17b956b5346d5540ec0cb398d83df5d8a8a22
iltStudent07 <iltStudent07@labs.webagesolutions.com>
2026-09-23 16:54:10 +0000

    Update Routes to include TaskDetail and ProjectDetail

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,9 @@ import ProtectedRoute from './components/ProtectedRoute'
 import { Navigate, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
+import ProjectDetail from './pages/ProjectDetail'
 import Tasks from './pages/Tasks'
+import TaskDetail from './pages/TaskDetail'
 import Login from './pages/Login'
 import NotFound from './pages/NotFound'
 import Register from './pages/Register'
@@ -21,7 +23,9 @@ function App() {
               <Route index element={<Navigate to='/dashboard' replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/projects" element={<Projects />} />
+              <Route path="/projects/:id" element={<ProjectDetail />} />
               <Route path="/tasks" element={<Tasks />} />
+              <Route path="/tasks/:id" element={<TaskDetail />} />
             </Route>
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
